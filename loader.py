b9e1cfa65a3e615dc497195251301fd37350491f
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:41 +0500

    .

--- loader.py
+++ loader.py
@@ -1,8 +1,16 @@
+# Import necessary modules
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+# Import configuration data
 from data import config
 
+# Create an instance of the Bot class with the bot token and specified parse mode
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+
+# Create an instance of the MemoryStorage class for storing data
 storage = MemoryStorage()
+
+# Create an instance of the Dispatcher class, passing the bot and storage objects
 dp = Dispatcher(bot, storage=storage)
+
