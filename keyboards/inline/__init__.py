85ab5f650daaf62b3030cfd329f511c9bcc6eb2f
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:44 +0500

    .

--- keyboards/inline/__init__.py
+++ keyboards/inline/__init__.py
@@ -1,1 +1,8 @@
-
+# This function calculates the factorial of a number
+def factorial(n):
+    # If the input is 0 or 1, return 1
+    if n == 0 or n == 1:
+        return 1
+    else:
+        # Otherwise, recursively calculate the factorial
+        return n * factorial(n-1)
