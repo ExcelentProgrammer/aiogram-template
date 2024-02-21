dda7d664ef727618d11c75d74f0498305b667803
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:43 +0500

    .

--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,8 +1,12 @@
+# Importing necessary modules and classes
 from aiogram import Dispatcher
 
-from loader import dp
-from .throttling import ThrottlingMiddleware
+from loader import dp # Importing dp from loader.py
+from .throttling import ThrottlingMiddleware # Importing ThrottlingMiddleware from throttling.py
 
 
+# Checking if the current module is being run directly or being imported as a module
 if __name__ == "middlewares":
+    
+    # Adding ThrottlingMiddleware as a middleware to the Dispatcher object
     dp.middleware.setup(ThrottlingMiddleware())
