f42804d6f7dadc07bd8bbe8f12f863a7802b6920
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:38 +0500

    .

--- data/config.py
+++ data/config.py
@@ -1,9 +1,3 @@
+# Importing the Env class from the environs library
 from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
