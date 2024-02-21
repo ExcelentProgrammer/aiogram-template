ddd2a504a29ffd7bc05abb48bf0aef849cbfc56b
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:41 +0500

    .

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from aiogram import executor
+from aiogram import executor  # Importing the executor module from aiogram
 
-from loader import dp
-import middlewares, filters, handlers
-from utils.notify_admins import on_startup_notify
-from utils.set_bot_commands import set_default_commands
+from loader import dp  # Importing the dispatcher from loader
+import middlewares, filters, handlers  # Importing middlewares, filters and handlers
+from utils.notify_admins import on_startup_notify  # Importing the on_startup_notify function from notify_admins
+from utils.set_bot_commands import set_default_commands  # Importing the set_default_commands function from set_bot_commands
 
-
+# This is an asynchronous function that runs when the bot starts up
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/star va /help)
+    # This sets the default commands for the bot
     await set_default_commands(dispatcher)
 
-    # Bot ishga tushgani haqida adminga xabar berish
+    # This sends a notification to the admins when the bot starts up
     await on_startup_notify(dispatcher)
 
-
 if __name__ == '__main__':
+    # This starts the polling loop for the bot
     executor.start_polling(dp, on_startup=on_startup)
