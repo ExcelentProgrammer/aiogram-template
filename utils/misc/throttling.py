99f56786d39b324050826337331f3900345c8941
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:45 +0500

    .

--- utils/misc/throttling.py
+++ utils/misc/throttling.py
@@ -2,15 +2,28 @@ def rate_limit(limit: int, key=None):
     """
     Decorator for configuring rate limit and key in different functions.
 
-    :param limit:
-    :param key:
-    :return:
+    This decorator can be used to set a rate limit and a key for a function. 
+    The rate limit determines how many times the function can be called within a certain time period.
+    The key is an optional parameter that can be used to uniquely identify the caller.
+
+    :param limit: The rate limit for the function.
+    :param key: An optional key to uniquely identify the caller.
+    :return: The decorated function.
     """
 
+    # The decorator function takes a function as an argument and returns a new function
+    # that has additional attributes set for rate limiting.
     def decorator(func):
+        # Set the rate limit for the function
         setattr(func, 'throttling_rate_limit', limit)
+
+        # If a key was provided, set it as an attribute for the function
         if key:
             setattr(func, 'throttling_key', key)
+
+        # Return the original function, which has been modified with new attributes
         return func
 
+    # Return the decorator function, which can be used to decorate other functions
     return decorator
+
