56273c54826aaeff0b911276a3191a48965032a0
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:40 +0500

    .

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,4 +1,14 @@
+# Import the errors module, which contains custom error classes and exceptions.
 from . import errors
+
+# Import the users module, which contains functions and classes related to managing
+# user accounts and authentication.
 from . import users
+
+# Import the groups module, which contains functions and classes related to managing
+# user groups and permissions.
 from . import groups
+
+# Import the channels module, which contains functions and classes related to managing
+# communication channels and messages between users.
 from . import channels
