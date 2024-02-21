90c6f72a2c1bc973c5054bbb0098340e48a73d69
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:39 +0500

    .

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,14 +1,5 @@
-from aiogram import types
-from aiogram.dispatcher.filters.builtin import CommandHelp
+# Importing necessary modules
+from aiogram import types  # for handling messages and other telegram objects
+from aiogram.dispatcher.filters.builtin import CommandHelp  # for handling help command
+from loader import dp  # for initializing the dispatcher
 
-from loader import dp
-
-
-@dp.message_handler(CommandHelp())
-async def bot_help(message: types.Message):
-    text = ("Qanday yordam kerak?",
-            "Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam")
-    
-    await message.answer("\n".join(text))
