5bacf5be6f63003ff44b1488f4bc499f58ff23e3
Programmer <106369143+ExcelentProgrammer@users.noreply.github.com>
2024-02-22 01:34:43 +0500

    .

--- utils/misc/logging.py
+++ utils/misc/logging.py
@@ -1,5 +1,14 @@
+# Import the logging module
 import logging
 
-logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
-                    level=logging.INFO,
-                    )
+# Configure the basic settings for the logging module
+# The basicConfig method sets up a simple, basic configuration for the logging module
+logging.basicConfig(
+    # Set the format of the log messages
+    # The format string contains various placeholders that will be replaced with actual values when a log message is created
+    format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',
+
+    # Set the root logging level to INFO
+    # This means that only messages with a level of INFO or higher will be logged
+    level=logging.INFO,
+)
